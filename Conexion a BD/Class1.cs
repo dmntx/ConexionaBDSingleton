0b1bc21b73c85957f395d6988794167b1a433625
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-02-26 20:28:33 -0600

    Agregar archivos de proyecto.

---
+++ Conexion a BD/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion_a_BD
+{
+	class Conexion
+	{
+		private bool conected;
+		private static Conexion uniqueInstance;
+		private static readonly object InstanceLoker = new Object();
+
+		private Conexion()
+		{
+			conected = false;
+		}
+
+		public static Conexion getInstance()
+		{
+			if (uniqueInstance == null)
+			{
+				lock (InstanceLoker)
+				{
+					if (uniqueInstance == null)
+						Console.WriteLine("Creacion de unica instancia de Chocolate Boiler");
+					uniqueInstance = new Conexion();
+				}
+			}
+			Console.WriteLine("Regresando la instancia de Conexion");
+			return uniqueInstance;
+		}
+
+		public void conectar()
+        {
+            if (!conected)
+            {
+				conected = true;
+				Console.WriteLine("Conectado a la base de datos");
+            }
+            else
+            {
+				Console.WriteLine("Ya estás conectado");
+            }
+        }
+		public void desconectar()
+        {
+            if (conected)
+            {
+				conected = false;
+				Console.WriteLine("Desconectado de la base de datos");
+            }
+            else
+            {
+				Console.WriteLine("Ya estás desconectado");
+            }
+        }
+	}
+}
+
---
+++ Conexion a BD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion_a_BD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conexion nuevaBD = Conexion.getInstance();
+            Conexion base2 = Conexion.getInstance();
+
+            nuevaBD.conectar(); //Conectado la bd desde el primer objeto
+            base2.conectar(); //intentando conectar la bd desde el segundo objeto
+
+            base2.desconectar(); //desconectado la bd desde el segundo objeto
+            nuevaBD.desconectar(); //intentando desconectar la bd desde el primero objeto
+            Console.ReadKey();
+        }
+    }
+}
